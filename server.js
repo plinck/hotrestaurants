7bcd442219d4369c33321c9b882f77e85d0d1a4c
Remy <remington49@rocketmail.com>
2019-02-16 13:21:53 -0500

    new

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ var path = require("path");
 var app = express();
 var PORT = 3000;
 
+var tables = [
+    
+  ];
+  var waitlist = [
+   
+  ];
+  
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -29,7 +37,60 @@ app.get("/tables", function(req, res) {
     res.sendFile(path.join(__dirname, "public/reserve.html"));
   });
 
+  app.get("/api/tables", function(req, res) {
+    return res.json(tables);
+  });
   
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
+  // Displays waitlist
+  app.get("/api/waitlist", function(req, res) {
+    return res.json(waitlist);
+  });
+  
+  // Displays a single table, or returns false
+  app.get("/api/tables/:table", function(req, res) {
+    var table = req.params.table;
+  
+    console.log(table);
+  
+    for (let i in tables) {
+      if (table === tables[i].routeName) {
+        return res.json(tables[i]);
+      }
+    }
+  
+    return res.json(false);
+  });
+  
+  app.post("/api/tables", function(req, res) {
+    let newReservation = req.body;
+    console.log(newReservation);
+  
+    let newTable = {};
+  
+    if (tables.length < 5) {
+      newTable.routeName = (`table${tables.length}`)
+      newTable.routeName = newTable.routeName.replace(/\s+/g, "").toLowerCase();
+  
+      newTable.number = (`Table #${tables.length}`);
+      newTable.customerName = newReservation.customerName;
+      newTable.phoneNumber = newReservation.phoneNumber;
+      newTable.customerEmail = newReservation.customerEmail;
+      newTable.customerID = newReservation.customerID;
+  
+      console.log(newTable);
+      tables.push(newTable);
+  
+      res.json(newTable);
+    } else {
+      waitlist.push(newReservation);
+      
+      res.end()
+    }
+  });
+  
+  // Starts the server to begin listening
+  // =============================================================
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
